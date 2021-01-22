64a6eb613aa874ab17d80218b9d19950384451b6
sheasahli <shea.sahli@gmail.com>
2021-01-22 10:14:59 -0800

    alert javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 
+const alertBanner = document.getElementById('alert');
 
 /*----- Alert -----*/
+
+alertBanner.innerHTML =
+`
+  <div class="alertBanner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-close">x</p>
+  </div>
+  `;
+
+  alertBanner.addEventListener('click', e => {
+    if(e.target.className === 'alert-close'){
+      alertBanner.style.display = "none";
+    }
+  });
